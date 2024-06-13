5100b89258d031c009f0cfb4743e6dad4d296add
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-06-13 16:04:04 +0100

    url and views

--- account/models.py
+++ account/models.py
@@ -9,13 +9,16 @@ class Account(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     pin = models.CharField(max_length=4)
-    balance = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     ACCOUNT_TYPE = [
         ('SAV', 'SAVING'),
         ('CUR', 'CURRENT'),
         ('DOM', 'DOMICILIARY'),
     ]
     account_type = models.CharField(max_length=3, choices=ACCOUNT_TYPE, default='SAV')
+
+    # def __str__(self):
+    #     return f"{self.first_name} {self.last_name} {self.account_type} {self.balance}"
 
 
 class Transaction(models.Model):
--- accountManagementSystem/urls.py
+++ accountManagementSystem/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Account Management System'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("demo/", include("demo.urls"))
