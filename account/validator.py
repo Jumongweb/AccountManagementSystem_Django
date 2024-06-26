9f55bbea8d2ba77c78f5c33d155b91eedb9b263f
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-06-26 12:12:53 +0100

    validator

--- account/validator.py
+++ account/validator.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 
 
-def validate_pin(pin):
+def validate_pin(pin:str):
     if len(pin) < 4:
         return ValidationError("Pin must be at least 4")
