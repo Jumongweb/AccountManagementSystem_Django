6c3338f2638d1711e036372612ea981db21f5629
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-07-02 16:33:37 +0100

    user view.py

---
+++ user/views.py
@@ -0,0 +1,13 @@
+# from django.shortcuts import render
+# from rest_framework.generics import CreateAPIView
+#
+# from user.models import User
+# from user.serializers import UserCreateSerializer
+#
+#
+# # Create your views here.
+#
+# class UserRegistration(CreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserCreateSerializer
+#
